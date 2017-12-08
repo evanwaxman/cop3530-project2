a7875d58ae59e30006336599f1415f2e1fb423b3
evanwaxman <waxman.evan@gmail.com>
2017-12-08 13:45:34 -0500

    updated test code for BSTRAND

--- catch_test.cpp
+++ catch_test.cpp
@@ -12,6 +12,12 @@
 
 #include <iostream>
 #include "BSTLEAF.h"
+#include "BSTROOT.h"
+#include "BSTRAND.h"
+
+#define TEST_BSTLEAF 1
+//#define TEST_BSTROOT 1
+//#define TEST_BSTRAND 1
 
 using namespace cop3530;
 
@@ -36,8 +42,12 @@ bool is_equal(const int& key1, const int& key2) {
 }
 
 TEST_CASE("Insert components into tree") {
-    BSTLEAF<int, char, compare, is_equal>* map = new BSTLEAF<int, char, compare, is_equal>;
 
+#ifdef TEST_BSTLEAF
+    std::cout << "------------- TESTING BSTLEAF --------------" << std::endl;
+    
+    BSTLEAF<int, char, compare, is_equal>* map = new BSTLEAF<int, char, compare, is_equal>;
+    
     map->insert(5,'w');
     REQUIRE( map->lookup(5) == 'w');
     
@@ -52,15 +62,91 @@ TEST_CASE("Insert components into tree") {
     
     map->insert(0, 'A');
     REQUIRE( map->lookup(0) == 'A');
-
+    
     map->insert(10, 'z');
     REQUIRE( map->lookup(10) == 'z');
+    
+    map->insert(20, 'k');
+    REQUIRE( map->lookup(20) == 'k');
+#elif TEST_BSTROOT
+    std::cout << "------------- TESTING BSTROOT --------------" << std::endl;
+    
+    BSTROOT<int, char, compare, is_equal>* map = new BSTROOT<int, char, compare, is_equal>;
+    
+    map->insert(5,'w');
+    REQUIRE( map->lookup(5) == 'w');
+    REQUIRE( map->get_root() == 5);
+    
+    map->insert(3,'a');
+    REQUIRE( map->lookup(3) == 'a');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(9,'e');
+    REQUIRE( map->lookup(9) == 'e');
+    REQUIRE( map->get_root() == 9);
+    
+    map->insert(1,'y');
+    REQUIRE( map->lookup(1) == 'y');
+    REQUIRE( map->get_root() == 1);
+    
+    map->insert(0, 'A');
+    REQUIRE( map->lookup(0) == 'A');
+    REQUIRE( map->get_root() == 0);
+    
+    map->insert(10, 'z');
+    REQUIRE( map->lookup(10) == 'z');
+    REQUIRE( map->get_root() == 10);
+    
+    map->insert(20, 'k');
+    REQUIRE( map->lookup(20) == 'k');
+    REQUIRE( map->get_root() == 20);
+#elif TEST_BSTRAND
+    std::cout << "------------- TESTING BSTRAND --------------" << std::endl;
+    
+    BSTRAND<int, char, compare, is_equal>* map = new BSTRAND<int, char, compare, is_equal>;
+    
+    map->insert(5,'w');
+    REQUIRE( map->lookup(5) == 'w');
+    REQUIRE( map->get_root() == 5);
+    
+    map->insert(3,'a');
+    REQUIRE( map->lookup(3) == 'a');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(9,'e');
+    REQUIRE( map->lookup(9) == 'e');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(1,'y');
+    REQUIRE( map->lookup(1) == 'y');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(0, 'A');
+    REQUIRE( map->lookup(0) == 'A');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(10, 'z');
+    REQUIRE( map->lookup(10) == 'z');
+    REQUIRE( map->get_root() == 3);
+    
+    map->insert(20, 'k');
+    REQUIRE( map->lookup(20) == 'k');
+    REQUIRE( map->get_root() == 20);
+#endif
     
     delete map;
 }
 
+
 TEST_CASE("Remove components from tree") {
+    
+#ifdef TEST_BSTLEAF
     BSTLEAF<int, char, compare, is_equal>* map = new BSTLEAF<int, char, compare, is_equal>;
+#elif TEST_BSTROOT
+    BSTROOT<int, char, compare, is_equal>* map = new BSTROOT<int, char, compare, is_equal>;
+#elif TEST_BSTRAND
+    BSTRAND<int, char, compare, is_equal>* map = new BSTRAND<int, char, compare, is_equal>;
+#endif
     
     map->insert(5,'w');
     map->insert(1,'A');
@@ -71,7 +157,7 @@ TEST_CASE("Remove components from tree") {
     map->insert(6,'l');
     
     map->remove(5);
-    //REQUIRE_THROWS(map->lookup(5));
+    REQUIRE_THROWS(map->lookup(5));
     
     map->remove(1);
     REQUIRE_THROWS( map->lookup(1));
@@ -93,3 +179,4 @@ TEST_CASE("Remove components from tree") {
     
     delete map;
 }
+
